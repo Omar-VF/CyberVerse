2fa2ded4043706b86f8a5f5b04f4afe990d2ed2f
Shreyansh Soy <shreyanshsoy07@gmail.com>
2025-09-22 16:13:05 +0530

    ..

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function startRecording() {
       const a = document.createElement('a');
       document.body.appendChild(a);
       a.style = 'display:none';
-      a.href = url;
+      a.href = url;  
       a.download = 'gmeet-recording.webm';
       a.click();
       window.URL.revokeObjectURL(url);
